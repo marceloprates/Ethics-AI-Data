99a33a7d6c034fe3843b4d7b83cc2afe1727044e
dragonavelar <dragonavelar@gmail.com>
2017-11-16 15:53:20 -0200

    Renaming files and deleting not used script

--- Code/gen_csvs.py
+++ Code/gen_csvs.py
@@ -1,23 +1,20 @@
 import sys
 from lxml import etree
 
-with open( "venues.txt", "r", encoding = "utf-8" ) as f:
-	VENUES = f.read().splitlines()
-#end with
 with open( "venuesb.txt", "r", encoding = "utf-8" ) as f:
 	BROAD_VENUES = f.read().splitlines()
 #end with
-with open( "keywords.txt", "r", encoding = "utf-8" ) as f:
-	KEYWORDS = f.read().splitlines()
-#end with
 
-CSV_SEP = ';'
+CSV_SEP = '\t'
 PATH_TO_XML = "./dblp.xml"
 CATEGORIES = set(
 	["article", "inproceedings", "proceedings", "book", "incollection", "phdthesis", "mastersthesis", "www"]
 )
-SKIP_CATEGORIES = set(["phdthesis", "mastersthesis", "www"])
-DATA_ITEMS = ["title", "booktitle", "year", "journal", "ee"]#, "volume"]
+DATA_ITEMS = ["title", "booktitle", "journal", "volume", "year", "ee"]
+TDATA_ITEMS = ["key", "tag", "title", "booktitle", "journal", "volume", "year", "ee"]
+JDATA_ITEMS = ["key", "title", "journal", "volume", "year", "ee"]
+CDATA_ITEMS = ["key", "title", "booktitle", "year", "ee"]
+DBLP_SIZE = 11792843
 
 def clear_element(element):
 	element.clear()
@@ -39,142 +36,88 @@ def extract_paper_elements( iter_parser, log_file = sys.stderr ):
 	#end try
 #end extract_paper_elements
 
-def write_header(csv_file,end='\n'):
-	csv_file.write( "dblpkey{CS}tag{CS}mdate".format( CS = CSV_SEP ) )
-	for data_item in DATA_ITEMS:
-		csv_file.write( "{CS}{}".format( data_item, CS = CSV_SEP ) )
+def write_header(csv_file,data_items,end='\n'):
+	for data_item in data_items[:-1]:
+		csv_file.write( "{}{CS}".format( data_item, CS = CSV_SEP ) )
+	csv_file.write( "{}".format( data_items[-1], CS = CSV_SEP ) )
 	csv_file.write( end )
 #end write_header
 
-def write_entry(paper,csv_file,end='\n'):
-	csv_file.write( "{dblpkey}{CS}{tag}{CS}{mdate}".format(
-		dblpkey = paper["dblpkey"],
-		tag = paper["element"],
-		mdate = paper["mdate"],
-		CS = CSV_SEP)
-	)
-	for data_item in DATA_ITEMS:
+def write_entry(paper,csv_file,data_items,end='\n'):
+	for data_item in data_items[:-1]:
 		if type( paper[ data_item ] ) is str:
-			item = "{CS}\"{}\"".format( ''.join( c for c in paper[ data_item ] if c not in ['"', CSV_SEP] ), CS = CSV_SEP )
+			item = "\"{}\"{CS}".format( paper[ data_item ].replace('"','').replace(CSV_SEP,''), CS = CSV_SEP )
 		else:
-			item = "{CS}{}".format( paper[ data_item ], CS = CSV_SEP )
+			item = "{}{CS}".format( paper[ data_item ], CS = CSV_SEP )
 		#end if
 		csv_file.write( item )
 	#end for
+	if type( paper[ data_items[-1] ] ) is str:
+		item = "\"{}\"{CS}".format( paper[ data_items[-1] ].replace('"','').replace(CSV_SEP,''), CS = CSV_SEP )
+	else:
+		item = "{}{CS}".format( paper[ data_items[-1] ], CS = CSV_SEP )
+	#end if
+	csv_file.write( item )
 	csv_file.write( end )
 #end write_entry
 
-def check_text(text,keywords):
-	for keyword in keywords:
-		if keyword is not None and text is not None and keyword in text:
-			return True
-		#end if
-	#end for
-	return False
-#end check_text
-
-def read_from_write_to( iter_parser, csv_file, log_file = sys.stderr ):
-	for venue in VENUES:
-		if venue is None:
-			continue
-		#end if
-		fname = ''.join( c for c in venue if c.isalnum() )
-		with open( "venues/{venue}.txt".format( venue=fname ), "w", encoding = "utf-8" ) as f:
-			write_header( f, end='' )
-			f.write( "{FS}contains\n".format( FS = CSV_SEP ) )
-		#end with
-	#end for
-	for venue in BROAD_VENUES:
-		if venue is None:
-			continue
-		#end if
-		fname = ''.join( c for c in venue if c.isalnum() )
-		with open( "bvenues/{venue}.txt".format( venue=fname ), "w", encoding = "utf-8" ) as f:
-			write_header( f, end='' )
-			f.write( "{FS}contains\n".format( FS = CSV_SEP ) )
-		#end with
-	#end for
-	#found_confs = set()
-	#found_books = set()
-	#end for
+def read_from_write_to( iter_parser, journal_csv_file, conference_csv_file, trash_csv_file, log_file = sys.stderr ):
+	write_header( journal_csv_file, JDATA_ITEMS )
+	write_header( conference_csv_file, CDATA_ITEMS )
+	write_header( trash_csv_file, TDATA_ITEMS )
 	for paperCounter, element in enumerate( extract_paper_elements( iter_parser ) ):
-		authors = [ author.text for author in element.findall( "author" ) ]
 		if element.get("key") is None or element.tag is None:
 			continue
 		#end if
 		paper = {
-			"element" : element.tag,
+			"tag" : element.tag,
 			"mdate" : element.get("mdate"),
-			"dblpkey" : element.get("key")
+			"key" : element.get("key")
 		}
 		for data_item in DATA_ITEMS:
 			data = element.find(data_item)
 			if data is not None:
 				try:
-					paper[ data_item ] = data.text
+					paper[ data_item ] = '' if data.text is None else data.text
 				except AttributeError:
 					paper[ data_item ] = data
-				#end try
-				#if data_item == "journal":
-				#	found_confs.add( paper[ data_item ] )
-				#elif data_item == "booktitle":
-				#	found_books.add( paper[ data_item ] )
-				#end if
 			else:
 				paper[ data_item ] = ""
 			#end if
 		#end for
-		if paper[ "element" ] not in SKIP_CATEGORIES:
-			for venue in VENUES:
-				if venue is None:
-					continue
+		for venue in BROAD_VENUES:
+			bt = '' if paper["booktitle"] is None else paper["booktitle"]
+			j = '' if paper["journal"] is None else paper["journal"]
+			if venue in bt or venue in j:
+				if paper["tag"] == "inproceedings":
+					write_entry( paper, conference_csv_file, CDATA_ITEMS )
+				elif paper["tag"] == "article":
+					write_entry( paper, journal_csv_file, JDATA_ITEMS )
+				else:
+					write_entry( paper, trash_csv_file, TDATA_ITEMS )
 				#end if
-				fname = ''.join( c for c in venue if c.isalnum() )
-				with open( "venues/{venue}.txt".format( venue=fname ), "a", encoding = "utf-8" ) as f:
-					v = venue
-					bt = '' if paper["booktitle"] is None else paper["booktitle"]
-					j = '' if paper["journal"] is None else paper["journal"]
-					if v == bt or v == j:
-						contains = check_text( paper["title"], KEYWORDS )
-						write_entry( paper, f, end='')
-						f.write( "{CS}{}\n".format( 1 if contains else 0, CS = CSV_SEP ) )
-					#end if
-			#end for
-			for venue in BROAD_VENUES:
-				if venue is None:
-					continue
-				#end if
-				fname = ''.join( c for c in venue if c.isalnum() )
-				with open( "bvenues/{venue}.txt".format( venue=fname ), "a", encoding = "utf-8" ) as f:
-					v = venue
-					bt = '' if paper["booktitle"] is None else paper["booktitle"]
-					j = '' if paper["journal"] is None else paper["journal"]
-					if v in bt or v in j:
-						contains = check_text( paper["title"], KEYWORDS )
-						write_entry( paper, f, end='')
-						f.write( "{CS}{}\n".format( 1 if contains else 0, CS = CSV_SEP ) )
-					#end if
-				#end with
-			#end for
+				break
+			#end if
+		#end for
+		if paperCounter % 1000 == 0:
+			print(
+				"{counter}\t{percentage:.2f}%".format(
+					counter = paperCounter,
+					percentage = 100 * paperCounter / DBLP_SIZE),
+				file=sys.stdout
+			)
 		#end if
-		print( "{counter}".format( counter = paperCounter ), file=sys.stdout )
 	#end for
-	#with open( "found_confs.txt", "w", encoding = "utf-8" ) as f:
-	#	for conf in found_confs:
-	#		f.write( "{conf}\n".format( conf = conf ) )
-	#	#end for
-	#end with
-	#with open( "found_books.txt", "w", encoding = "utf-8" ) as f:
-	#	for book in found_books:
-	#		f.write( "{book}\n".format( book = book ) )
-	#	#end for
-	#end with
 #end read_from_write_to
 
 def main():
-	with open( "dblp.csv", mode = "w", encoding = "utf-8" ) as csv_file:
-		iter_parser = etree.iterparse(PATH_TO_XML, dtd_validation=True, events=("start", "end"))
-		read_from_write_to(iter_parser, csv_file)
+	with open( "jdblp.csv", mode = "w", encoding = "utf-8" ) as journal_csv_file:
+		with open( "cdblp.csv", mode = "w", encoding = "utf-8" ) as conference_csv_file:
+			with open( "trash.csv", mode = "w", encoding = "utf-8" ) as trash_csv_file:
+				iter_parser = etree.iterparse(PATH_TO_XML, dtd_validation=True, events=("start", "end"))
+				read_from_write_to(iter_parser, journal_csv_file, conference_csv_file, trash_csv_file)
+			#end with
+		#end with
 	#end with
 #end main
 
