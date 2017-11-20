3172d15a80b117d9407e94a6e36c3e8825ee3504
Marcelo Prates <marceloorp@gmail.com>
2017-11-20 09:43:20 -0200

    Fixed minor details in the Haskell scripts

--- Code/abstract-parser.hs
+++ Code/abstract-parser.hs
@@ -21,19 +21,17 @@ findAbstract = maximumBy (comparing length)
 
 main = do
 
-	ethics		<- (sort . lines) <$> readFile "dblp/ethics_keywords.txt"
-	classical	<- (sort . lines) <$> readFile "dblp/classical_keywords.txt"
-	trending	<- (sort . lines) <$> readFile "dblp/trending_keywords.txt"
-
-	forbidden	<- words <$> readFile "abstracts/forbidden_wordcloud.txt"
+	ethics		<- (sort . lines) <$> readFile "../DBLP/ethics_keywords.txt"
+	classical	<- (sort . lines) <$> readFile "../DBLP/classical_keywords.txt"
+	trending	<- (sort . lines) <$> readFile "../DBLP/trending_keywords.txt"
 
 	let keywords = ethics
 
-	entries_aaai 					<- map (\x -> (replace ";" "" $ replace "\n" "" $ x!!0, findAbstract x)) <$> (chunksOf 10 . init . splitOn "#") <$> readFile "abstracts/aaai.txt"
-	entries_nips					<- map (\x -> (replace ";" "" $ replace "\n" "" $ x!!0, findAbstract x)) <$> (chunksOf 10 . init . splitOn "#") <$> readFile "abstracts/nips.txt"
+	entries_aaai 					<- map (\x -> (replace ";" "" $ replace "\n" "" $ x!!0, findAbstract x)) <$> (chunksOf 10 . init . splitOn "#") <$> readFile "../Abstracts/aaai.txt"
+	entries_nips					<- map (\x -> (replace ";" "" $ replace "\n" "" $ x!!0, findAbstract x)) <$> (chunksOf 10 . init . splitOn "#") <$> readFile "../Abstracts/nips.txt"
 
 	let entries_keywords 			= map (\(year,abstract) -> (year, find_keywords abstract keywords, length $ words $ abstract)) entries_nips
 	let entries_keywords_byyear 	= map (\x -> (fst3 $ head x, (fromIntegral $ sum $ accumulate $ map snd3 x)/(fromIntegral $ length x)::Double )) $ groupBy (\a b -> (<=0) $ abs $ (read $ fst3 a) - (read $ fst3 b)) $ sortBy (comparing fst3) entries_keywords 
 
-	--writeFile "abstracts/aaai-abstract-texts.txt" $ intercalate " " $ filter (\word -> (not $ all isDigit word) && (not $ elem (map toLower word) forbidden)) $ words $ replace "]" " " $ replace "[" " " $ replace ")" " " $ replace "(" " " $ filter (not . (`elem` "1234567890,.?!:;\"\'")) $ concat $ map snd entries_aaai
+	writeFile "stats/abstracts-aaai-ethics.dat" $ unlines $ map (\x -> printf "%s %0.3f" (fst x) (snd x) ) entries_keywords_byyear
 	writeFile "stats/abstracts-nips-ethics.dat" $ unlines $ map (\x -> printf "%s %0.3f" (fst x) (snd x) ) entries_keywords_byyear
--- Code/title-parser.hs
+++ Code/title-parser.hs
@@ -19,20 +19,16 @@ accumulate = foldr (zipWith (+)) (repeat 0)
 
 main = do
 
-	ethics		<- (sort . lines) <$> readFile "dblp/ethics_keywords.txt"
-	classical	<- (sort . lines) <$> readFile "dblp/classical_keywords.txt"
-	trending	<- (sort . lines) <$> readFile "dblp/trending_keywords.txt"
+	ethics		<- (sort . lines) <$> readFile "../DBLP/ethics_keywords.txt"
+	classical	<- (sort . lines) <$> readFile "../DBLP/classical_keywords.txt"
+	trending	<- (sort . lines) <$> readFile "../DBLP/trending_keywords.txt"
 
-	let keywords = trending
+	let keywords = ethics
 
-	cdblp <- map (\x -> let y = map (replace "\"" "") $ splitOn "\t" x in (y!!1,replace "/" "" $ replace " " "-" $ y!!2,y!!3) ) <$> drop 1 . lines <$> readFile "dblp/cdblp.csv"
-	jdblp <- map (\x -> let y = map (replace "\"" "") $ splitOn "\t" x in (y!!1,replace "/" "" $ replace " " "-" $ y!!2,y!!4) ) <$> drop 1 . lines <$> readFile "dblp/jdblp.csv"
+	cdblp <- map (\x -> let y = map (replace "\"" "") $ splitOn "\t" x in (y!!1,replace "/" "" $ replace " " "-" $ y!!2,y!!3) ) <$> drop 1 . lines <$> readFile "../DBLP/cdblp.csv"
+	jdblp <- map (\x -> let y = map (replace "\"" "") $ splitOn "\t" x in (y!!1,replace "/" "" $ replace " " "-" $ y!!2,y!!4) ) <$> drop 1 . lines <$> readFile "../DBLP/jdblp.csv"
 
 	let 
-		--cdblp_aggregated = map (\(t,v,y) -> if (aaai_symposium v) then (t,"AAAI-Symposium",y) else if (aaai_workshop v) then (t,"AAAI-Workshop",y) else (t,v,y)) cdblp where
-		--	aaai_symposium v = (isInfixOf "AAAI-Fall-Symposium" v) || (isInfixOf "AAAI-Spring-Symposium" v)
-		--	aaai_workshop v = isInfixOf "AAAI-Workshop" v
-
 		cdblp_aggregated = map (\(t,v,y) -> if ai_conf v then (t,"AI",y) else if robotics_conf v then (t,"Robotics",y) else (t,v,y)) cdblp where
 			ai_conf v 		= elem v ["AAAI","IJCAI","NIPS","ICML"]
 			robotics_conf v = elem v ["ICRA","IROS"]
@@ -60,20 +56,5 @@ main = do
 		cdblp_keywords_byvenue = map (\x -> (snd3 $ head x, map (\y -> (thd3 $ head y, (map ((/(fromIntegral $ length y)) . fromIntegral) $ accumulate $ map fst3 y)::[Double] )) $ groupBy (\a b -> (<=5) $ abs $ (read $ thd3 a)-(read $ thd3 b) ) $ sortBy (comparing thd3) x )) $ groupBy (\a b -> (snd3 a) == (snd3 b)) $ sortBy (comparing snd3) $ map (\x -> (find_keywords (fst3 x) keywords, snd3 x, thd3 x)) cdblp_aggregated
 		jdblp_keywords_byvenue = map (\x -> (snd3 $ head x, map (\y -> (thd3 $ head y, (map ((/(fromIntegral $ length y)) . fromIntegral) $ accumulate $ map fst3 y)::[Double] )) $ groupBy (\a b -> (<=5) $ abs $ (read $ thd3 a)-(read $ thd3 b) ) $ sortBy (comparing thd3) x )) $ groupBy (\a b -> (snd3 a) == (snd3 b)) $ sortBy (comparing snd3) $ map (\x -> (find_keywords (fst3 x) keywords, snd3 x, thd3 x)) jdblp_aggregated
 
-		cdblp_keywords_byyear = map (\x -> (thd3 $ head x, map (\y -> (snd3 $ head y, (((/(fromIntegral $ length y)) . fromIntegral) $ sum $ accumulate $ map (\z -> find_keywords (fst3 z) keywords) y)::Double, sum $ accumulate $ map (\z -> find_keywords (fst3 z) keywords) y )) $ groupBy (\a b -> (snd3 a)==(snd3 b)) $ sortBy (comparing snd3) x )) $ groupBy (\a b -> (thd3 a) == (thd3 b)) $ sortBy (comparing thd3) $ filter (\(t,v,y) -> selected v) cdblp_aggregated
-		jdblp_keywords_byyear = map (\x -> (thd3 $ head x, map (\y -> (snd3 $ head y, (((/(fromIntegral $ length y)) . fromIntegral) $ sum $ accumulate $ map (\z -> find_keywords (fst3 z) keywords) y)::Double, sum $ accumulate $ map (\z -> find_keywords (fst3 z) keywords) y )) $ groupBy (\a b -> (snd3 a)==(snd3 b)) $ sortBy (comparing snd3) x )) $ groupBy (\a b -> (thd3 a) == (thd3 b)) $ sortBy (comparing thd3) $ filter (\(t,v,y) -> selected v) jdblp_aggregated
-
-		selected v =
-			v == "Artif.-Intell."
-			{-v == "AAAI" 	||
-			v == "IJCAI" 	||
-			v == "NIPS" 	||
-			v == "ICML" 	||
-			v == "ICRA" 	||
-			v == "IROS"-}
-
-	print $ filter ((\x -> elem x ["Artif.-Intell."]) . fst) $ map (\x -> (snd3 $ head x, length x)) $ groupBy (\a b -> (==) (snd3 a) (snd3 b)) $ sortBy (comparing snd3) $ jdblp_aggregated
-
-	--writeFile "table-test.dat" $ unlines $ map (\(y,x) -> printf "%s %.2f/ %d" y (snd3 $ head x) (thd3 $ head x) ) jdblp_keywords_byyear
-	--mapM_ (\(venue,history) -> writeFile (printf "stats/conf/trending/%s" venue)		( unlines $ (intercalate " " $ "year":keywords) : map (\(year,frequencies) -> let frequencies' = (sum frequencies) : frequencies in year ++ " " ++ (intercalate " " $ map show frequencies') ) history) ) cdblp_keywords_byvenue
-	--mapM_ (\(venue,history) -> writeFile (printf "stats/journal/trending/%s" venue)		( unlines $ (intercalate " " $ "year":keywords) : map (\(year,frequencies) -> let frequencies' = (sum frequencies) : frequencies in year ++ " " ++ (intercalate " " $ map show frequencies') ) history) ) jdblp_keywords_byvenue
+	mapM_ (\(venue,history) -> writeFile (printf "stats/conf/%s" venue)			( unlines $ (intercalate " " $ "year":keywords) : map (\(year,frequencies) -> let frequencies' = (sum frequencies) : frequencies in year ++ " " ++ (intercalate " " $ map show frequencies') ) history) ) cdblp_keywords_byvenue
+	mapM_ (\(venue,history) -> writeFile (printf "stats/journal/%s" venue)		( unlines $ (intercalate " " $ "year":keywords) : map (\(year,frequencies) -> let frequencies' = (sum frequencies) : frequencies in year ++ " " ++ (intercalate " " $ map show frequencies') ) history) ) jdblp_keywords_byvenue
