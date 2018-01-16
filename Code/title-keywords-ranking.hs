087d0c45f80996e60c04cc26bc684d36ae0eb563
Marcelo Prates <marceloorp@gmail.com>
2018-01-16 12:55:30 -0200

    Converted strings to lowercase

--- Code/title-keywords-ranking.hs
+++ Code/title-keywords-ranking.hs
@@ -12,7 +12,7 @@ import Data.Ord
 import qualified Data.Text as T
 
 {- Custom list of special characters which are to be removed from each string in an attempt to "clean" them -}
-special_characters = ['.',',']
+special_characters = ['.',',','-']
 
 clean_string = filter (\x -> not $ elem x special_characters)
 
@@ -21,8 +21,8 @@ main = do
 	cdblp <- map (\x -> let y = map (replace "\"" "") $ splitOn "\t" x in y!!1 ) <$> drop 1 . lines <$> readFile "../DBLP/cdblp.csv"
 	jdblp <- map (\x -> let y = map (replace "\"" "") $ splitOn "\t" x in y!!1 ) <$> drop 1 . lines <$> readFile "../DBLP/jdblp.csv"
 
-	let words_cdblp = map clean_string $ concat $ map words $ filter ((>0) . length) cdblp
-	let words_jdblp = map clean_string $ concat $ map words $ filter ((>0) . length) cdblp
+	let words_cdblp = map ((map toLower) . clean_string) $ concat $ map words $ filter ((>0) . length) cdblp
+	let words_jdblp = map ((map toLower) . clean_string) $ concat $ map words $ filter ((>0) . length) cdblp
 
 	writeFile "title-keywords-ranking-conf.dat" 	$ unlines $ map (\x -> printf "%s %d" (head x) (length x)) $ sortBy (comparing length) $ groupBy (==) $ sort words_cdblp
 	writeFile "title-keywords-ranking-journal.dat" 	$ unlines $ map (\x -> printf "%s %d" (head x) (length x)) $ sortBy (comparing length) $ groupBy (==) $ sort words_jdblp
